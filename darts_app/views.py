7c3501754d40d1801dca67540231548598633c14
wyeth0725 <c125054ycomwork.c.oka-pu.ac.jp>
2019-09-20 00:10:14 +0900

    add template

--- darts_app/views.py
+++ darts_app/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+    #order by の引数の前に-つけるとdescになる
+    latest_question_list = Question.objects.all().order_by("-publish_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return render(request, "darts_app/index.html", context)
