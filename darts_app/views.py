72e9f0db333bbb240c6812183edb601d0d5f3bb2
wyeth0725 <c125054ycomwork.c.oka-pu.ac.jp>
2019-09-24 23:42:22 +0900

    fix

--- darts_app/views.py
+++ darts_app/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 from .models import Question
@@ -5,7 +8,24 @@ from .models import Question
 def index(request):
     #order by の引数の前に-つけるとdescになる
     latest_question_list = Question.objects.all().order_by("-publish_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
+    context = {"latest_question_list": latest_question_list}
     return render(request, "darts_app/index.html", context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "darts_app/detail.html", {"question": question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "darts_app/detail.html", {"question": question, "error_message": "You didn't select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("darts_app:results", args=(question.id)))
